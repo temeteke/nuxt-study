92f52d40833afa04fe639639b503203d2c9ca994
temeteke <temeteke@gmail.com>
2024-06-29 05:21:29 +0000

    Add todo app

---
+++ pages/todo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <h1 class="title">ToDo</h1>
+        <input type="text" v-model="input">
+        <button @click="add">add</button>
+        <li v-for="todo in todos">
+            {{ todo }}
+        </li>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+export default class App extends Vue{
+    input: string = '';
+    todos: string[] = [];
+
+    add() {
+        this.todos.push(this.input)
+    };
+}
+</script>
