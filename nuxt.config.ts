8fa80414e62ded6019b5ad15a8be3fb95b128a8e
temeteke <temeteke@gmail.com>
2024-06-29 10:23:59 +0000

    Add ESLint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   vite: {
     server: {
       watch: {
         usePolling: true,
       }
     }
-  }
+  },
+
+  modules: ["@nuxt/eslint"]
 })
