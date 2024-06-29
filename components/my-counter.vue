1414954a9fc623251485d39994daca5b6dbbde68
temeteke <temeteke@gmail.com>
2024-06-29 13:07:46 +0000

    Change my-counter.vue

--- components/my-counter.vue
+++ components/my-counter.vue
@@ -1,15 +1,12 @@
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue'
 
-// リアクティブな状態
 const count = ref(0)
 
-// 状態を変更し更新トリガーする関数
-function increment() {
+const increment = () => {
   count.value++
 }
 
-// ライフサイクルフック
 onMounted(() => {
   console.log(`The initial count is ${count.value}.`)
 })
