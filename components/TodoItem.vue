25af88f0582a07c5ecd23bdae048443f1ba5786d
temeteke <temeteke@gmail.com>
2024-07-01 13:07:05 +0000

    Add TodoItem

---
+++ components/TodoItem.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+interface Props {
+  index: number
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <li><span>{{ index }}.</span><span><slot /></span></li>
+</template>
---
+++ components/TodoList.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const input = ref<string>('')
+const todos = ref<string[]>([])
+
+const add = () => {
+  todos.value.push(input.value)
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      <UInput
+        v-model="input"
+        type="text"
+      />
+      <UButton @click="add">
+        add
+      </UButton>
+    </div>
+    <ul>
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :index="index"
+      >
+        {{ todo }}
+      </TodoItem>
+    </ul>
+  </div>
+</template>
